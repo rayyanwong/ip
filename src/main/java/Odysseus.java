d08f0f7cfc6ddc1b8ae24bf69677c87a72f6679c
Rayyan <rwrayyan2004@gmail.com>
2026-08-19 21:49:59 +0800

    Level-0: rename chatbot to Odysseus and add greet/exit with new banner

--- src/main/java/Odysseus.java
+++ src/main/java/Odysseus.java
@@ -1,7 +1,14 @@
 public class Odysseus {
     private static final String NAME = "Odysseus";
     public static void main(String[] args) {
+
         String str = String.format("""
+           ___      _                             \s
+          / _ \\  __| |_   _ ___ ___  ___ _   _ ___\s
+         | | | |/ _` | | | / __/ __|/ _ \\ | | / __|
+         | |_| | (_| | |_| \\__ \\__ \\  __/ |_| \\__ \\
+          \\___/ \\__,_|\\__, |___/___/\\___|\\__,_|___/
+                      |___/                       \s
         ____________________________________________________________
         Hello! I'm %s
         What can I do for you?
