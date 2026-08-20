76ed68b8bdaca40682fc02c34f3100194584ae38
Rayyan <rwrayyan2004@gmail.com>
2026-08-20 10:47:18 +0800

    Level-3: add Task class and mark/unmark support

--- src/main/java/Odysseus.java
+++ src/main/java/Odysseus.java
@@ -15,11 +15,13 @@ public class Odysseus {
         ____________________________________________________________
         """;
 
+    private static final String MARK_MSG = "Nice! I've marked this task as done:%n  %s";
+    private static final String UNMARK_MSG = "OK, I've marked this task as not done yet:%n  %s";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean chatting = true;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
 
         String str = String.format("""
@@ -38,26 +40,50 @@ public class Odysseus {
 
         while (chatting) {
             String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
-                chatting = false;
-                System.out.println(BYE_MSG);
-            } else if (input.equalsIgnoreCase("list")) {
-                // list out tasks
-                StringBuilder sb = new StringBuilder();
-                if (count == 0) {
-                    System.out.println(String.format(MSG_FORMAT, "No tasks available"));
-                } else {
-                    for (int i = 0; i < count; i++) {
-                        sb.append((i + 1) + ". " + tasks[i] + "\n");
-                    }
-                    System.out.println(String.format(MSG_FORMAT, sb.toString()));
+            String command = input.split(" ")[0];
+            switch (command.toLowerCase()) {
+
+                case "bye" -> {
+                    chatting = false;
+                    System.out.println(BYE_MSG);
                 }
-            } else {
-                // add task
-                tasks[count] = input;
-                count++;
-                String addedMsg = String.format("added: %s", input);
-                System.out.println(String.format(MSG_FORMAT, addedMsg));
+
+                case "list" -> {
+                    // list out tasks
+                    StringBuilder sb = new StringBuilder();
+                    if (count == 0) {
+                        System.out.println(String.format(MSG_FORMAT, "No tasks available"));
+                    } else {
+                        for (int i = 0; i < count; i++) {
+                            sb.append((i + 1) + ". " + tasks[i].toString() + "\n");
+                        }
+                        System.out.println(String.format(MSG_FORMAT, sb.toString()));
+                    }
+                }
+
+                case "mark" -> {
+                    // TODO: error handling for invalid index
+                    int idx = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasks[idx].markAsDone();
+                    String msg = String.format(MARK_MSG, tasks[idx]);
+                    System.out.println(String.format(MSG_FORMAT, msg));
+                }
+
+                case "unmark" -> {
+                    // TODO: error handling for invalid index
+                    int idx = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasks[idx].markAsUndone();
+                    String msg = String.format(UNMARK_MSG, tasks[idx]);
+                    System.out.println(String.format(MSG_FORMAT, msg));
+                }
+
+                default -> {
+                    Task newTask = new Task(input);
+                    tasks[count] = newTask;
+                    count++;
+                    String addedMsg = String.format("added: %s", newTask.toString());
+                    System.out.println(String.format(MSG_FORMAT, addedMsg));
+                }
             }
         }
     }
