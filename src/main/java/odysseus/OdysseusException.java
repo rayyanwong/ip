b1b4a38160dc6c06e567116da5da9b41b289c46e
Rayyan <rwrayyan2004@gmail.com>
2026-08-27 15:22:44 +0800

    A-JavaDoc: Add JavaDoc for OdysseusException

--- src/main/java/odysseus/OdysseusException.java
+++ src/main/java/odysseus/OdysseusException.java
@@ -1,7 +1,13 @@
 package odysseus;
 
-/** An error caused by invalid user input to odysseus.Odysseus */
+/** An error caused by invalid user input to Odysseus. */
 public class OdysseusException extends Exception {
+
+    /**
+     * Constructs an {@link OdysseusException} with the given detail message.
+     *
+     * @param msg string representing details of the error
+     */
     public OdysseusException(String msg) {
         super(msg);
     }
