e98393297c28f8fd10e888c99672191c80329681
Rayyan <rwrayyan2004@gmail.com>
2026-08-27 15:26:32 +0800

    A-JavaDoc: Add JavaDoc for Ui class

--- src/main/java/odysseus/ui/Ui.java
+++ src/main/java/odysseus/ui/Ui.java
@@ -21,6 +21,7 @@ public class Ui {
         ____________________________________________________________
         """;
 
+    /** Constructs the Ui instance. */
     public Ui() {
         this.name = "Odysseus";
     }
